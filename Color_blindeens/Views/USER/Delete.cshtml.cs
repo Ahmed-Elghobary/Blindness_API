22415eb367c558da0f4dc910c6313fae1c34d905
norhan khaled <norhank252@gmail.com>
2024-04-22 18:42:55 +0200

    creat app color_blindess create crud app

---
+++ Color_blindeens/Views/USER/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Color_blindeens.Data;
+using Color_blindeens.Models;
+
+namespace Color_blindeens.Views.USER
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Color_blindeens.Data.Color_blindeensContext _context;
+
+        public DeleteModel(Color_blindeens.Data.Color_blindeensContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.User.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Color_blindeens/Views/USER/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Color_blindeens.Data;
+using Color_blindeens.Models;
+
+namespace Color_blindeens.Views.USER
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Color_blindeens.Data.Color_blindeensContext _context;
+
+        public IndexModel(Color_blindeens.Data.Color_blindeensContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
